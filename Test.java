fdc599cc64a0407a076f1baee6e6e93ea612c5ff
Kota <n.delong1991@gmail.com>
2024-01-31 01:03:47 -0500

    New lines in src code.

--- Test.java
+++ Test.java
@@ -1,5 +1,24 @@
+import java.io.*;
+
 public class Test{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args)
+        throws IOException
+    {
+        int myClassNum;
+        BufferedReader classID = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("What is the Class ID?");
+        
+        String IDClass = classID.readLine();
+
+        BufferedReader classNum = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("What is the class number?");
+
+        String numClass = classNum.readLine();
+        
+        myClassNum = Integer.valueOf(numClass);
+        
+        System.out.println("Hello World!");
+        System.out.print("Your class is " + IDClass);
+        System.out.print(" " + myClassNum);
     }
 }
